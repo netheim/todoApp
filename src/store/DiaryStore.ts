ad1aa134cbd1041b5a706f2bba84d605249a9ad1
netheim <alfizik05@gmail.com>
2025-02-07 15:14:52 +0300

    update

--- src/store/DiaryStore.ts
+++ src/store/DiaryStore.ts
@@ -1,36 +1,39 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import {TaskTypes} from "../types/Task.types.ts";
-
-type NotesTypes = {
-    id: string;
-    title: string;
-    text: string;
-    time: string;
-}
-
-interface DiaryStore {
-    notes: NotesTypes[];
-    addNote: (title: string, text: string, time: string) => void;
-}
-
-const useDiaryStore = create<DiaryStore>()(persist
-    (set => ({
-        notes: [],
-        addNote: (title, time) =>
-            set((state) => {
-                const newTask: TaskTypes = {
-                    id: String(Date.now()),
-                    title,
-                    text,
-                    time,
-
-
-
-                };
-                return { notes: [...state.notes, newTask] };
-            }),
-    })
-));
-
-export default useDiaryStore;
+// import { create } from "zustand";
+// import { persist } from "zustand/middleware";
+// import {TaskTypes} from "../types/Task.types.ts";
+//
+// type NotesTypes = {
+//     id: string;
+//     title: string;
+//     text: string;
+//     time: string;
+// }
+//
+// interface DiaryStore {
+//     notes: NotesTypes[];
+//     addNote: (title: string, text: string, time: string) => void;
+// }
+//
+// const useDiaryStore = create<DiaryStore>()(persist
+//     (set => ({
+//         notes: [],
+//         addNote: (title, time) =>
+//             set((state) => {
+//                 const newTask: TaskTypes = {
+//                     id: String(Date.now()),
+//                     title,
+//                     text,
+//                     time,
+//
+//
+//
+//                 };
+//                 return { notes: [...state.notes, newTask] };
+//             }),
+//     }),
+//         {
+//             name: "active-tasks-storage", // Ключ для localStorage
+//         }
+// ));
+//
+// export default useDiaryStore;
